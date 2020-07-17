ba6ac89a4185407205236b477cc12aaf67110aea
Otis Guess <guess01325@gmail.com>
2020-07-17 16:34:04 -0700

    [dom material]

---
+++ app.js
@@ -0,0 +1,180 @@
+let sentence = 'Today it is Friday';
+console.log.sentence
+
+
+let Age = 34
+console.log
+
+let faveTeam = [lakers, celtics,supersonc,pistons,Bulls];
+console.log
+let boolean = true;
+console.log(boolean);
+
+
+
+
+// make 10 differnet functions
+// one of your functions need to pass in an array
+// one of your functios need to pass in a callback
+// ... string
+// ..objects
+//boolean
+// ... freestyle
+function team() {
+    team [laker,boston,celtics,bulls]
+}
+function name () {
+    name = "Otis Guess"
+}
+function determine() {
+    team = true
+}
+function age() {
+    
+    Age = 32
+}
+function baseballTeam () {
+    [dodgers,redsox,cubs,orioles]
+}
+
+function person() {
+    name: "otis",
+    place: "new london"
+}
+
+
+
+doesThisReallyWork(doesThisWork);
+
+const sectionOne = document.getElementById('section-one')
+
+const sectionTwo = document.getElementById('section-two')
+console.log(sectionTwo)
+
+const sectionThree = document.querySelector('#section-three');
+console.log(sectionThree)
+
+const sectionFour = document.querySelector.("#section-four");
+console.log(sectionFour);
+
+const sectionFive = document.querySelector('#section-five');
+console.log(section-sectionFive);
+
+
+sectionOne.textContent = 'lakers';
+sectionTwo.textContent = 'celtis';
+sectionThree.textContent = 'wariors';
+sectionFour.textContent = 'pistons';
+sectionFive.textContent = 'Bulls';
+
+
+const paraOne = document.querySelector('p.paragraph-one');
+console.log(paraOne);
+paraOne.textContent = 'Lebron James'
+
+
+
+const paraTwo = document.querySelector('.patagraph-two');
+console.log(paraTwo);
+paraTwo.textContent = 'steph Curry';
+
+cont paraThree = document.querySelector('.paragraph-three');
+console.log(paraThree)
+paraThree.textContent = 'Kemba Walker'
+
+
+const paraFour = document.querySelector('.paragraph-four')
+console.log(paraFour);
+paraFour.textContent = 'Kyrie Irving'
+
+const paraFive = document.querySelector('.paragraph-five');
+console.log(paragraphFive);
+paraFour.textContent = 'Kris Dunn'
+
+
+const containerSix = document.createElement ('div');
+containerSix.classList.add( 'container-six');
+console.log(containerSix);
+
+ const sectionSix = document.createElement('h2')
+ console.log(sectionSix);
+
+ sectionSix.setAttribute('id', 'section-six');
+sectionSix.textContent = 'rockets'
+console.log(sectionSix);
+
+const paraSix = document.createElementNS('p')
+paraSix.classList.add('paragraph-six');
+paraSix.tectContent = "james Harden"
+
+const body = document.querySelector('body');
+console.log(body)
+
+body.appendChild(containerSix);
+containerSix.appendChild(sectionSix)
+
+conatinerSix.appendChild(paraSix);
+
+
+const sectionSeven = document.createElementnt('div');
+containerSeven.classList.add('container-seven');
+console.log(sectionSeven)
+
+const sectionSeven = document.createElement('h2')
+console.log(sectionSeven)
+
+ sectiionSeven.setAttribute('id','section-seven');
+ sectionSeven.textContent = 'Celtics'
+ console.log(sectionSeven)
+
+ const praSeven = document.createElementNS('p')
+ praSeven.classList.add('paragraph-seven')
+ praSeven.textContent = 'Kemba Walker'
+
+ const body = document.querySelector('body');
+
+ body.appendChild(containerSeven);
+ containerSeven.appendChild(praSeven)
+
+
+ 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5 variable of different types
+// string
+//number
+// array
+//boolean
+//object (4 differnt key value pairs) [sting], [number, [array,[boolean]]]
